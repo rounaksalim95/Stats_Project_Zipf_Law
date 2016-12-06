2cfc2502366b33cafbfedebdb6fb229e9bd5afe5
rounaksalim95 <rounaksalim@gmail.com>
2016-12-05 21:11:21 -0600

    Update script

--- project.py
+++ project.py
@@ -2,11 +2,20 @@ import glob
 
 def analyze(file):
 	f = open(file, 'r')
-	for word in 
+	return collections.Counter(itertools.chain(line.split() for line in f))
 
 
 
 def main(): 
 	list_of_files = glob.glob('./*.txt')
+	list_of_frequencies = []
 	for file in list_of_files: 
-		analyze(file)
+		list_of_frequencies.append(analyze(file))
+
+	for freq in list_of_frequencies: 
+		print max(freq, key = freq.get)
+
+
+
+if __name__ == "__main__":
+	main()
